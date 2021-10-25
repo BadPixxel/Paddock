0bd59d2b72876683cd6382aeb826c890204d9e9e
BadPixxel <eshop.bpaquier@gmail.com>
2021-10-25 18:41:34 +0200

    ADD: Deployments

--- scripts/build-env-file.sh
+++ scripts/build-env-file.sh
@@ -21,11 +21,23 @@ if [ ! -d "build" ]; then mkdir "build"; fi
 # Build Env File
 cp "scripts/.env.dist" "build/.env"
 # Setup Backup Location
-if [ -z "${PADDOCK_BACKUP}" ]; then PADDOCK_BACKUP=""; echo " => No Backup Location Defined"; fi
+if [ -z "${PADDOCK_BACKUP}" ];
+then
+  export PADDOCK_BACKUP="";
+  echo " => No Backup Location Defined";
+fi
 php -r "file_put_contents('./build/.env', str_replace('%PADDOCK_BACKUP%', '${PADDOCK_BACKUP}', file_get_contents('./build/.env')));"
 # Setup Default Mysql Url
-if [ -z "${DATABASE_URL}" ]; then DATABASE_URL="mysql://root:@localhost:3306"; echo " => No MySql Database Url Defined"; fi
+if [ -z "${DATABASE_URL}" ];
+then
+  export DATABASE_URL="mysql://root:@localhost:3306";
+  echo " => No MySql Database Url Defined. Using ${DATABASE_URL}";
+fi
 php -r "file_put_contents('./build/.env', str_replace('%DATABASE_URL%', '${DATABASE_URL}', file_get_contents('./build/.env')));"
 # Setup Default Mongodb Url
-if [ -z "${MONGODB_URL}" ]; then MONGODB_URL="mongodb://localhost:27017/config"; echo " => No Mongodb Database Url Defined";  fi
+if [ -z "${MONGODB_URL}" ];
+then
+  export MONGODB_URL="mongodb://localhost:27017/config";
+  echo " => No Mongodb Database Url Defined. Using ${MONGODB_URL}";
+fi
 php -r "file_put_contents('./build/.env', str_replace('%MONGODB_URL%', '${MONGODB_URL}', file_get_contents('./build/.env')));"
