66471c33c6903843e76e1a022891f7b583e665a6
BadPixxel <eshop.bpaquier@gmail.com>
2022-02-08 03:39:00 +0100

    FIX: Commas in yml deploy

--- scripts/build-deploy-script.sh
+++ scripts/build-deploy-script.sh
@@ -19,5 +19,5 @@ echo "\033[32m PADDOCK DEPLOY --> Build SSH Script \033[0m"
 
 echo "echo '`cat scripts/deploy-paddock.sh`' > /tmp/deploy-paddock.sh;"
 echo "echo '`cat build/.env`' > /tmp/.env;"
-echo "echo '`cat build/paddock.yml`' > /tmp/paddock.yml;"
+echo "echo \"`cat build/paddock.yml`\" > /tmp/paddock.yml;"
 echo "bash /tmp/deploy-paddock.sh; exit;"
