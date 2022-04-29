0c6a868537ea5de3189526ca895a3ea6b87cf185
BadPixxel <eshop.bpaquier@gmail.com>
2022-04-29 14:19:05 +0200

    WIP: Proxmox NRPE Scripts

--- scripts/deploy-proxmox.sh
+++ scripts/deploy-proxmox.sh
@@ -26,7 +26,7 @@ echo "/==============================/  ";
 # Binaries
 DIR_PLUGINS="/usr/lib/nagios/plugins"
 DIR_CONFIG="/etc/nagios/nrpe.d"
-
+URL_PLUGINS="https://raw.githubusercontent.com/BadPixxel/Paddock/master/src/Apps/Nrpe/Resources/plugins"
 ################################################################
 # Read User Inputs
 echo "VM IDs to monitor? i.e 100,200,300"
@@ -35,19 +35,36 @@ VMIDs=${VMIDs:-100}
 echo "Backup name? Default 'backup-server'"
 read  BACKUP_NAME
 BACKUP_NAME=${BACKUP_NAME:-backup-server}
-echo "Ok, let's monitor VMs $VMIDs for '$BACKUP_NAME' Backups"
+echo "[OK] Let's monitor VMs $VMIDs for '$BACKUP_NAME' Backups !"
 ################################################################
-# Import
-
-if [ ! -d "/etc" ]; then
-    echo "[KO] Directory /etc does not exits"
-    exit 0
-fi
-
-
+# Import NRPE Plugins
 if [ ! -d "$DIR_PLUGINS" ]; then
     echo "[KO] Directory $DIR_PLUGINS does not exits"
     exit 0
 fi
-
-
+curl -s -o "$DIR_PLUGINS/check_prx_backups.sh" "$URL_PLUGINS/check_prx_backups.sh"
+curl -s -o "$DIR_PLUGINS/check_prx_status.sh" "$URL_PLUGINS/check_prx_status.sh"
+echo "[OK] Plugins Downloaded to $DIR_PLUGINS"
+################################################################
+# Generate NRPE Config File
+if [ ! -d "$DIR_CONFIG" ]; then
+    echo "[KO] Directory $DIR_CONFIG does not exits"
+    exit 0
+fi
+CONFIG_FILE="$DIR_CONFIG/paddock_prx.cfg"
+echo "################################################################" >  $CONFIG_FILE
+echo "#                PADDOCK NRPE CONFIG FOR PROXMOX               #" >> $CONFIG_FILE
+echo "################################################################" >> $CONFIG_FILE
+echo "# This file was auto-generated, so don't touch please !        #" >> $CONFIG_FILE
+echo "################################################################" >> $CONFIG_FILE
+echo "command[check_prx_backups]=/usr/lib/nagios/plugins/check_prx_backups.sh $VMIDs 3 $BACKUP_NAME" >> $CONFIG_FILE
+echo "command[check_prx_status]=/usr/lib/nagios/plugins/check_prx_status.sh $VMIDs" >> $CONFIG_FILE
+echo "[OK] NRPE Config Generated to $CONFIG_FILE"
+################################################################
+# Restart NRPE Service
+if [ ! -f "/etc/init.d/nagios-nrpe-server" ]; then
+    echo "[KO] NRPE Service was not found"
+    exit 0
+fi
+/etc/init.d/nagios-nrpe-server restart
+echo "[OK] NRPE Service Restarted"
