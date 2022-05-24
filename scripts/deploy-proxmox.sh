9e6935369b6ccdfd0f2b6cdf4c2678c8f71de712
BadPixxel <eshop.bpaquier@gmail.com>
2022-05-24 13:13:55 +0200

    ADD: PXR Version Checks

--- scripts/deploy-proxmox.sh
+++ scripts/deploy-proxmox.sh
@@ -28,15 +28,6 @@ DIR_PLUGINS="/usr/lib/nagios/plugins"
 DIR_CONFIG="/etc/nagios/nrpe.d"
 URL_PLUGINS="https://raw.githubusercontent.com/BadPixxel/Paddock/master/src/Apps/Nrpe/Resources/plugins"
 ################################################################
-# Read User Inputs
-echo "VM IDs to monitor? i.e 100,200,300"
-read  VMIDs
-VMIDs=${VMIDs:-100}
-echo "Backup name? Default 'backup-server'"
-read  BACKUP_NAME
-BACKUP_NAME=${BACKUP_NAME:-backup-server}
-echo "[OK] Let's monitor VMs $VMIDs for '$BACKUP_NAME' Backups !"
-################################################################
 # Import NRPE Plugins
 if [ ! -d "$DIR_PLUGINS" ]; then
     echo "[KO] Directory $DIR_PLUGINS does not exits"
@@ -46,6 +37,8 @@ curl -s -o "$DIR_PLUGINS/check_prx_backups.sh" "$URL_PLUGINS/check_prx_backups.s
 chmod 775 "$DIR_PLUGINS/check_prx_backups.sh"
 curl -s -o "$DIR_PLUGINS/check_prx_status.sh" "$URL_PLUGINS/check_prx_status.sh"
 chmod 775 "$DIR_PLUGINS/check_prx_status.sh"
+curl -s -o "$DIR_PLUGINS/check_prx_version.sh" "$URL_PLUGINS/check_prx_version.sh"
+chmod 775 "$DIR_PLUGINS/check_prx_version.sh"
 echo "[OK] Plugins Downloaded to $DIR_PLUGINS"
 ################################################################
 # Generate NRPE Config File
@@ -53,15 +46,24 @@ if [ ! -d "$DIR_CONFIG" ]; then
     echo "[KO] Directory $DIR_CONFIG does not exits"
     exit 0
 fi
-CONFIG_FILE="$DIR_CONFIG/paddock_prx.cfg"
-echo "################################################################" >  $CONFIG_FILE
-echo "#                PADDOCK NRPE CONFIG FOR PROXMOX               #" >> $CONFIG_FILE
-echo "################################################################" >> $CONFIG_FILE
-echo "# This file was auto-generated, so don't touch please !        #" >> $CONFIG_FILE
-echo "################################################################" >> $CONFIG_FILE
-echo "command[check_prx_backups]=/usr/lib/nagios/plugins/check_prx_backups.sh $VMIDs 3 $BACKUP_NAME" >> $CONFIG_FILE
-echo "command[check_prx_status]=/usr/lib/nagios/plugins/check_prx_status.sh $VMIDs" >> $CONFIG_FILE
-echo "[OK] NRPE Config Generated to $CONFIG_FILE"
+CONFIG_SAMPLE="$DIR_CONFIG/paddock_prx.cfg"
+CONFIG_FILE="$DIR_CONFIG/paddock_prx.cfg.sample"
+echo "################################################################" >  $CONFIG_SAMPLE
+echo "#                PADDOCK NRPE CONFIG FOR PROXMOX               #" >> $CONFIG_SAMPLE
+echo "################################################################" >> $CONFIG_SAMPLE
+echo "# This file was auto-generated, so don't touch please !        #" >> $CONFIG_SAMPLE
+echo "################################################################" >> $CONFIG_SAMPLE
+echo "command[check_prx_backups]=/usr/lib/nagios/plugins/check_prx_backups.sh 100 3 backup-server" >> $CONFIG_SAMPLE
+echo "command[check_prx_status]=/usr/lib/nagios/plugins/check_prx_status.sh 100" >> $CONFIG_SAMPLE
+echo "command[check_prx_version]=/usr/lib/nagios/plugins/check_prx_version.sh 7.0" >> $CONFIG_SAMPLE
+echo "[OK] NRPE Config Generated to $CONFIG_SAMPLE"
+################################################################
+# Install Config File
+if [ ! -f "$CONFIG_FILE" ]; then
+    cp $CONFIG_SAMPLE $CONFIG_FILE
+    echo "[OK] NRPE Config Created at $CONFIG_FILE"
+    exit 0
+fi
 ################################################################
 # Restart NRPE Service
 if [ ! -f "/etc/init.d/nagios-nrpe-server" ]; then
---
+++ src/Apps/Nrpe/Resources/plugins/check_prx_version.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+################################################################################
+#
+# Copyright (C) 2020 BadPixxel <www.badpixxel.com>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+################################################################################
+
+readonly SCRIPTNAME="$(basename "${BASH_SOURCE[0]}")"
+
+######################################
+# HELP
+function usage() {
+        echo "./check_prx_version.sh MinVersion"
+        echo
+        echo "MinVersion:   Minimal Server Version"
+        echo
+        echo "Options:"
+        echo "  -h, --help    Show this help message and exit."
+        echo
+        echo "Examples:"
+        echo "$SCRIPTNAME 2.1.0"
+        exit 1
+}
+######################################
+# VERSIONS COMPARE FUNCTIONS
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+######################################
+# Safety Checks
+if [[ -z $1 || "$1" = "-h" || "$1" = "--help" ]]; then
+    usage
+fi
+######################################
+# Read PVE Version
+VERSION=$(pveversion | grep "manager" | cut -d'/' -f 2 | cut -d'-' -f 1);
+echo $VERSION
+######################################
+# Compare Version
+if version_ge $VERSION $1; then
+    echo "OK - PVE Version $VERSION is above $1"
+    exit 0
+fi
+
+echo "KO - PVE Version $VERSION is lower to $1"
+exit 1
