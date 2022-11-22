eed85ef34d657e0b262ad51abfa232b97ab9e83d
BadPixxel <eshop.bpaquier@gmail.com>
2022-11-22 14:53:14 +0100

    WIP: Add Entity Integrity Tracks

--- config/bundles.php
+++ config/bundles.php
@@ -12,21 +12,27 @@
  */
 
 return array(
+    //====================================================================//
+    // Paddock Core Bundles
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array('all' => true),
-
     BadPixxel\Paddock\Core\CoreBundle::class => array('all' => true),
-    BadPixxel\Paddock\System\Php\PaddockPhpBundle::class => array('all' => true),
+    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => array('all' => true),
+
+    //====================================================================//
+    // Paddock MySql Bundles
     BadPixxel\Paddock\System\MySql\PaddockMySqlBundle::class => array('all' => true),
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => array('all' => true),
+
+    //====================================================================//
+    // Paddock MongoDb Bundles
     BadPixxel\Paddock\System\MongoDb\PaddockMongoDbBundle::class => array('all' => true),
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => array('all' => true),
+
+    //====================================================================//
+    // Paddock Other Extensions Bundle
+    BadPixxel\Paddock\System\Php\PaddockPhpBundle::class => array('all' => true),
     BadPixxel\Paddock\System\Shell\PaddockShellBundle::class => array('all' => true),
     BadPixxel\Paddock\Apps\Nrpe\PaddockNrpeBundle::class => array('all' => true),
     BadPixxel\Paddock\Apps\Sentry\PaddockSentryBundle::class => array('all' => true),
     BadPixxel\Paddock\Backup\BackupBundle::class => array('all' => true),
-
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => array('all' => true),
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => array('all' => true),
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => array('all' => true),
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => array('all' => true),
-
-    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => array('all' => true),
 );
