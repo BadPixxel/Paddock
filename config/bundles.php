173c8dcf9ebe1e506abd17577391b34fe68d9092
BadPixxel <eshop.bpaquier@gmail.com>
2022-11-28 22:10:08 +0100

    WIP: Paddock Project

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,24 @@
 <?php
 
-//====================================================================//
+/*
+ *  Copyright (C) BadPixxel <www.badpixxel.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 // CORE BUNDLES
 //====================================================================//
 $bundles = array(
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
-    BadPixxel\Paddock\Core\PaddockCoreBundle::class => ['all' => true],
-    BadPixxel\Paddock\PaddockProjectBundle::class => ['all' => true],
-    BadPixxel\Paddock\Backup\BackupBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array('all' => true),
+    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => array('all' => true),
+    BadPixxel\Paddock\Core\PaddockCoreBundle::class => array('all' => true),
+    BadPixxel\Paddock\PaddockProjectBundle::class => array('all' => true),
+    BadPixxel\Paddock\Backup\BackupBundle::class => array('all' => true),
 );
 
 //====================================================================//
@@ -17,8 +27,8 @@ $bundles = array(
 
 if (extension_loaded("pdo_mysql")) {
     $bundles = array_merge($bundles, array(
-        Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-        BadPixxel\Paddock\System\MySql\PaddockMySqlBundle::class => ['all' => true],
+        Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => array('all' => true),
+        BadPixxel\Paddock\System\MySql\PaddockMySqlBundle::class => array('all' => true),
     ));
 }
 
@@ -28,8 +38,8 @@ if (extension_loaded("pdo_mysql")) {
 
 if (extension_loaded("mongodb")) {
     $bundles = array_merge($bundles, array(
-        Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
-        BadPixxel\Paddock\System\MongoDb\PaddockMongoDbBundle::class => ['all' => true],
+        Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => array('all' => true),
+        BadPixxel\Paddock\System\MongoDb\PaddockMongoDbBundle::class => array('all' => true),
     ));
 }
 
@@ -38,8 +48,8 @@ if (extension_loaded("mongodb")) {
 //====================================================================//
 
 $bundles = array_merge($bundles, array(
-    BadPixxel\Paddock\Apps\Nrpe\PaddockNrpeBundle::class => ['all' => true],
-    BadPixxel\Paddock\Apps\Sentry\PaddockSentryBundle::class => ['all' => true],
+    BadPixxel\Paddock\Apps\Nrpe\PaddockNrpeBundle::class => array('all' => true),
+    BadPixxel\Paddock\Apps\Sentry\PaddockSentryBundle::class => array('all' => true),
 ));
 
 //====================================================================//
@@ -47,8 +57,8 @@ $bundles = array_merge($bundles, array(
 //====================================================================//
 
 $bundles = array_merge($bundles, array(
-    BadPixxel\Paddock\System\Php\PaddockPhpBundle::class => ['all' => true],
-    BadPixxel\Paddock\System\Shell\PaddockShellBundle::class => ['all' => true],
+    BadPixxel\Paddock\System\Php\PaddockPhpBundle::class => array('all' => true),
+    BadPixxel\Paddock\System\Shell\PaddockShellBundle::class => array('all' => true),
 ));
 
 return $bundles;
